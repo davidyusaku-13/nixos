90b5641d27dd46c1fd13ef52cd8b69846c895402
David Yusaku <davidyusaku13@gmail.com>
2026-07-11 16:20:16 +0700

    yeet

--- home.nix
+++ home.nix
@@ -12,7 +12,7 @@
     };
     profileExtra = ''
       if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-        exec uwsm start -S hyprland-uwsm.desktop
+        exec hyprland
       fi
     '';
   };
